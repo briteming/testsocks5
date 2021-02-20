d6b02c096a1bf973ab87d0c2ccd7b1b7e3458bd9
TxThinking <cloud@txthinking.com>
2021-02-21 03:01:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ fi
 
 mkdir _
 
-deno compile -A --unstable --target x86_64-unknown-linux-gnu -o _/testsocks5_linux_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
-deno compile -A --unstable --target x86_64-apple-darwin -o _/testsocks5_darwin_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
-deno compile -A --unstable --target x86_64-pc-windows-msvc -o _/testsocks5_windows_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target x86_64-unknown-linux-gnu -o _/testsocks5_linux_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target x86_64-apple-darwin -o _/testsocks5_darwin_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
+deno compile -A -r --unstable --target x86_64-pc-windows-msvc -o _/testsocks5_windows_amd64 https://raw.githubusercontent.com/brook-community/testsocks5/master/testsocks5.js
 
 nami release github.com/brook-community/testsocks5 $1 _
 
